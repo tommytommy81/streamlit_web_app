a0fd3adf3eb779ce00bdb30774eac8f60568ec57
tommytommy81 <tommy81.handy@gmail.com>
2025-09-03 13:14:58 +0200

    going back

--- streamlit_web_app.py
+++ streamlit_web_app.py
@@ -15,11 +15,14 @@ st.set_page_config(layout="wide")  # ← This expands your app to full width
 
 # Initialize dataframe
 df_choices = pd.read_csv('survey_results.csv', index_col=0)
-N_vouchers = pd.read_csv('N_vouchers.csv', index_col=0).at[0, 'N_vouchers']
+N_vouchers = pd.read_csv('N_vouchers.csv', index_col=0)
+print(N_vouchers)
 
-# Set the title
-st.title(f"""📊 Data Monitor (N = {N_vouchers}): 
-Who has completed the assessment? ({len(df_choices)}/{N_vouchers})
+# # Set the title
+# st.title(f"""📊 Data Monitor (N = {N_vouchers}): 
+# Who has completed the assessment? ({len(df_choices)}/{N_vouchers})
+
+st.title(f"""📊 Data Monitor (N = {len(df_choices)}): 
 
 Histograms for Each Question""")
 # Load data from local JSON files
